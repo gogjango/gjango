92e1ca906d914d6df33d3bcc3db467ab16a4e8fd
Calvin Cheng <linchuan.cheng@gmail.com>
2020-02-11 17:58:58 +0800

    TestRefresh in auth_test

--- service/auth_test.go
+++ service/auth_test.go
@@ -94,3 +94,69 @@ func TestLogin(t *testing.T) {
 		})
 	}
 }
+
+func TestRefresh(t *testing.T) {
+	cases := []struct {
+		name       string
+		req        string
+		wantStatus int
+		wantResp   *model.RefreshToken
+		userRepo   *mockdb.User
+		jwt        *mock.JWT
+	}{
+		{
+			name:       "Fail on FindByToken",
+			req:        "refreshtoken",
+			wantStatus: http.StatusInternalServerError,
+			userRepo: &mockdb.User{
+				FindByTokenFn: func(context.Context, string) (*model.User, error) {
+					return nil, apperr.DB
+				},
+			},
+		},
+		{
+			name:       "Success",
+			req:        "refreshtoken",
+			wantStatus: http.StatusOK,
+			userRepo: &mockdb.User{
+				FindByTokenFn: func(context.Context, string) (*model.User, error) {
+					return &model.User{
+						Username: "johndoe",
+						Active:   true,
+					}, nil
+				},
+			},
+			jwt: &mock.JWT{
+				GenerateTokenFn: func(*model.User) (string, string, error) {
+					return "jwttokenstring", mock.TestTime(2018).Format(time.RFC3339), nil
+				},
+			},
+			wantResp: &model.RefreshToken{Token: "jwttokenstring", Expires: mock.TestTime(2018).Format(time.RFC3339)},
+		},
+	}
+	gin.SetMode(gin.TestMode)
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			r := gin.New()
+			authService := auth.NewAuthService(tt.userRepo, tt.jwt)
+			service.AuthRouter(authService, r)
+			ts := httptest.NewServer(r)
+			defer ts.Close()
+			path := ts.URL + "/refresh/" + tt.req
+			res, err := http.Get(path)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer res.Body.Close()
+			if tt.wantResp != nil {
+				response := new(model.RefreshToken)
+				if err := json.NewDecoder(res.Body).Decode(response); err != nil {
+					t.Fatal(err)
+				}
+				assert.Equal(t, tt.wantResp, response)
+			}
+			assert.Equal(t, tt.wantStatus, res.StatusCode)
+		})
+	}
+}
