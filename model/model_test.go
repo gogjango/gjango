d93f13189f6e02c94ae07d12b834450623a08746
Calvin Cheng <linchuan.cheng@gmail.com>
2020-02-19 13:15:22 +0800

    Move our embedded postgres test into a proper test suite, and indicate that it is an integration test

--- model/model_test.go
+++ model/model_test.go
@@ -1,18 +1,10 @@
 package model_test
 
 import (
-	"log"
-	"path"
-	"path/filepath"
-	"runtime"
 	"testing"
 
 	"github.com/calvinchengx/gin-go-pg/mock"
 	"github.com/calvinchengx/gin-go-pg/model"
-	embeddedpostgres "github.com/fergusstrange/embedded-postgres"
-	"github.com/go-pg/pg/v9"
-	"github.com/go-pg/pg/v9/orm"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestBeforeInsert(t *testing.T) {
@@ -51,44 +43,4 @@ func TestDelete(t *testing.T) {
 		t.Errorf("DeletedAt not changed")
 	}
 
-}
-
-func TestDatabase(t *testing.T) {
-	_, b, _, _ := runtime.Caller(0)
-	d := path.Join(path.Dir(b))
-	projectRoot := filepath.Dir(d)
-	tmpDir := path.Join(projectRoot, "tmp")
-	testConfig := embeddedpostgres.DefaultConfig().
-		Username("db_test_user").
-		Password("db_test_password").
-		Database("db_test_database").
-		Version("12.1.0").
-		RuntimePath(tmpDir).
-		Port(9876)
-
-	postgres := embeddedpostgres.NewDatabase(testConfig)
-	err := postgres.Start()
-	assert.Equal(t, err, nil)
-
-	db := pg.Connect(&pg.Options{
-		Addr:     "localhost:9876",
-		User:     "db_test_user",
-		Password: "db_test_password",
-		Database: "db_test_database",
-	})
-	createSchema(db, &model.Company{}, &model.Location{}, &model.Role{}, &model.User{}, &model.Verification{})
-
-	_ = postgres.Stop()
-}
-
-func createSchema(db *pg.DB, models ...interface{}) {
-	for _, model := range models {
-		opt := &orm.CreateTableOptions{
-			IfNotExists: true,
-		}
-		err := db.CreateTable(model, opt)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
 }
---
+++ repository/account_integration_test.go
@@ -0,0 +1,77 @@
+package repository_test
+
+import (
+	"fmt"
+	"log"
+	"path"
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/calvinchengx/gin-go-pg/model"
+	"github.com/calvinchengx/gin-go-pg/repository"
+	embeddedpostgres "github.com/fergusstrange/embedded-postgres"
+	"github.com/go-pg/pg/v9"
+	"github.com/go-pg/pg/v9/orm"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"go.uber.org/zap"
+)
+
+type AccountTestSuite struct {
+	suite.Suite
+	db       *pg.DB
+	postgres *embeddedpostgres.EmbeddedPostgres
+}
+
+func (suite *AccountTestSuite) SetupTest() {
+	_, b, _, _ := runtime.Caller(0)
+	d := path.Join(path.Dir(b))
+	projectRoot := filepath.Dir(d)
+	tmpDir := path.Join(projectRoot, "tmp")
+	testConfig := embeddedpostgres.DefaultConfig().
+		Username("db_test_user").
+		Password("db_test_password").
+		Database("db_test_database").
+		Version(embeddedpostgres.V12).
+		RuntimePath(tmpDir).
+		Port(9876)
+
+	suite.postgres = embeddedpostgres.NewDatabase(testConfig)
+	err := suite.postgres.Start()
+	assert.Equal(suite.T(), err, nil)
+
+	suite.db = pg.Connect(&pg.Options{
+		Addr:     "localhost:9876",
+		User:     "db_test_user",
+		Password: "db_test_password",
+		Database: "db_test_database",
+	})
+	createSchema(suite.db, &model.Company{}, &model.Location{}, &model.Role{}, &model.User{}, &model.Verification{})
+}
+
+func (suite *AccountTestSuite) TearDownTest() {
+	suite.postgres.Stop()
+}
+
+func (suite *AccountTestSuite) TestAccount() {
+	log, _ := zap.NewDevelopment()
+	accountRepo := repository.NewAccountRepo(suite.db, log)
+	fmt.Println(accountRepo)
+}
+
+func TestAccountTestSuite(t *testing.T) {
+	suite.Run(t, new(AccountTestSuite))
+}
+
+func createSchema(db *pg.DB, models ...interface{}) {
+	for _, model := range models {
+		opt := &orm.CreateTableOptions{
+			IfNotExists: true,
+		}
+		err := db.CreateTable(model, opt)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
