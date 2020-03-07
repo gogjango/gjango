eaf263c2fcf1107c687cc4e67aedd6b00a194d6e
Calvin Cheng <linchuan.cheng@gmail.com>
2020-03-07 18:04:07 +0800

    Generate a cryptographically secure secret string as our jwt secret
    Signed-off-by: Calvin Cheng <linchuan.cheng@gmail.com>

---
+++ cmd/generate_secret.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/calvinchengx/gin-go-pg/secret"
+	"github.com/spf13/cobra"
+)
+
+// createCmd represents the migrate command
+var generateSecretCmd = &cobra.Command{
+	Use:   "generate_secret",
+	Short: "generate_secret",
+	Long:  `generate_secret`,
+	Run: func(cmd *cobra.Command, args []string) {
+		s, err := secret.GenerateRandomString(256)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("\nJWT_SECRET=%s\n\n", s)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(generateSecretCmd)
+}
