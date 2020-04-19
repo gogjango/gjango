4879e6163cee083f58b7203387617418464b7ab2
Calvin Cheng <linchuan.cheng@gmail.com>
2020-04-19 21:34:49 +0800

    Refactor as a library
    Signed-off-by: Calvin Cheng <linchuan.cheng@gmail.com>

---
+++ gjango.go
@@ -0,0 +1,10 @@
+package gjango
+
+import (
+	"github.com/gogjango/gjango/cmd"
+)
+
+// Run executes our gjango functions or servers
+func Run() {
+	cmd.Execute()
+}
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/gogjango/gjango/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
